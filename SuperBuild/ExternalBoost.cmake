9282401de59df06f6093d342534e344d1b72ce11
Kasper Marstal <kaspermarstal@gmail.com>
2015-10-29 04:24:32 -0700

    ENH: Fix BOOST SuperBuild

--- SuperBuild/ExternalBoost.cmake
+++ SuperBuild/ExternalBoost.cmake
@@ -19,22 +19,21 @@ else()
   endif()
 endif()
 
+set( BOOST_PREFIX_DIR "${CMAKE_INSTALL_PREFIX}/${PROJECT}-prefix" )
 set( BOOST_BUILD_DIR "${CMAKE_INSTALL_PREFIX}/${PROJECT}-build" )
 
 ExternalProject_Add( BOOST
-  BUILD_IN_SOURCE 1
   URL "${BOOST_URL}"
   URL_MD5 ${BOOST_MD5}
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND ${BOOST_CONFIGURE_COMMAND} 
-    --prefix=${BOOST_BUILD_DIR}/lib
   BUILD_COMMAND ${BOOST_BUILD_COMMAND} install
     --prefix=${BOOST_BUILD_DIR}
+    link=static
     --with-graph
     --variant=release
     --jobs=4
   INSTALL_COMMAND ""
 )
 
-#set( BOOST_INCLUDEDIR ${BOOST_BUILD_DIR}/include/boost-1_59 )
-set( BOOST_ROOT ${BOOST_BUILD_DIR} )
+set( BOOST_ROOT "${BOOST_PREFIX_DIR}/src/BOOST" )
