4ab98df1203850d26343203b67fb303c128b6675
Kasper Marstal <kaspermarstal@gmail.com>
2015-10-29 06:32:01 -0700

    COMP: Minimum viable example for testing declspecs

--- Modules/Core/Blueprints/include/elxBlueprint.h
+++ Modules/Core/Blueprints/include/elxBlueprint.h
@@ -12,48 +12,17 @@
 
 namespace elx {
 
-template< class TComponentDescriptor >
-class Blueprint : public itk::DataObject
+__declspec( dllexport ) class Blueprint : public itk::DataObject
 {
 public:
 
   elxNewMacro( Blueprint, itk::DataObject );
-
-  typedef TComponentDescriptor                                                ComponentDescriptorType;
-  typedef typename TComponentDescriptor::ComponentNameType                    ComponentNameType;
   
-  typedef boost::adjacency_list< boost::vecS,      
-                                 boost::vecS,      
-                                 boost::directedS,
-                                 ComponentDescriptorType >                    GraphType;
-
-  typedef typename boost::graph_traits< GraphType >::vertex_descriptor        ComponentType;
-  typedef typename boost::graph_traits< GraphType >::vertex_iterator          ComponentIterator, ComponentIteratorEnd;
-
-  typedef boost::vertex_index_t                                               ComponentIndexType;
-  typedef typename boost::property_map< GraphType, ComponentIndexType >::type ComponentIndexMapType;
-
-  typedef typename boost::graph_traits< GraphType >::edge_descriptor          ConnectionDescriptorType;
-  typedef typename boost::graph_traits< GraphType >::edge_iterator            ConnectionIterator, ConnectionIteratorEnd;
-
-  typedef typename boost::graph_traits< GraphType >::in_edge_iterator         InputIterator, InputIteratorEnd;
-  typedef typename boost::graph_traits< GraphType >::out_edge_iterator        OutputIterator, OutputIteratorEnd;
-
-  int TestFunction( void );
-  bool AddComponent( ComponentDescriptorType component );
-  bool SetComponent( ComponentIndexType componentIndex, ComponentDescriptorType component );
-  ComponentDescriptorType GetComponent( ComponentIndexType componentIndex );
-  bool RemoveComponent( ComponentDescriptorType component );
-
-  bool SetConnection( ComponentIndexType upstream, ComponentIndexType downstream );
-  ConnectionDescriptorType GetConnection( ConnectionDescriptorType Connection );
-  bool RemoveConnection( ConnectionDescriptorType connection );
-
-  void PrintGraph( void );
+  void PrintSelf( void );
 
 private:
 
-  GraphType m_Graph;
+  int m_Test = 0;
 
 };
 
--- Testing/Unit/elxBluePrintTest.cxx
+++ Testing/Unit/elxBluePrintTest.cxx
@@ -6,21 +6,6 @@ namespace elx {
 
 TEST( Blueprint, Instantiation )
 {
-  typedef Blueprint< ComponentDescriptor > BlueprintType;
-  BlueprintType::Pointer blueprint;
-  EXPECT_NO_THROW( blueprint = BlueprintType::New() );
-
-  typedef BlueprintType::ComponentDescriptorType ComponentDescriptorType;
-  ComponentDescriptorType::Pointer componentDescriptor;
-  EXPECT_NO_THROW( componentDescriptor = ComponentDescriptorType::New() );
-
-  typedef ComponentDescriptorType::ComponentNameType ComponentNameType;
-  ComponentNameType componentName;
-  EXPECT_NO_THROW( componentName = ComponentNameType("Metric") );
-  EXPECT_NO_THROW( componentDescriptor->SetComponentName( componentName ) );
-
-  EXPECT_NO_THROW( blueprint->TestFunction() );
-
   ASSERT_TRUE( true );
 }
 
