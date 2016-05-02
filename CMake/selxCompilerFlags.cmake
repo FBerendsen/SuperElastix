5b001f59928a35f64e32b14e24bfa6ec9ddafc89
Kasper Marstal <kaspermarstal@gmail.com>
2016-05-02 12:12:00 +0200

    ENH: Add the /MP flag when compiling with Visual Studio

--- CMake/selxCompilerFlags.cmake
+++ CMake/selxCompilerFlags.cmake
@@ -10,3 +10,22 @@ set( CompilerFlags
   CMAKE_C_FLAGS_RELEASE
   CMAKE_C_FLAGS_RELWITHDEBINFO
 )
+
+if(WIN32)
+  if(NOT MINGW)
+    if(CMAKE_GENERATOR MATCHES "^Visual Studio")
+      option(SUPERELASTIX_USE_MP "Add the /MP for object-file level parallel builds" OFF)
+      set(use_mp ${SUPERELASTIX_USE_MP})
+      foreach(listname ${compile_flag_lists})
+        if("${${listname}}" MATCHES "/Gm")
+          set(use_mp OFF)
+          message(AUTHOR_WARNING "/Gm is not compatible with /MP: Set SUPERELASTIX_USE_MP to OFF to avoid this warning.")
+        endif()
+      endforeach()
+      if(use_mp)
+        set(CMAKE_C_FLAGS "/MP ${CMAKE_C_FLAGS}")
+        set(CMAKE_CXX_FLAGS "/MP ${CMAKE_CXX_FLAGS}")
+      endif()
+    endif()
+  endif()
+endif()
